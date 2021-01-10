d74098ad137bd948e2105c564d4c5107cbc50140
merr1 <jabishvili31@gmail.com>
2021-01-11 03:04:12 +0400

    change style and add some feature

--- main.js
+++ main.js
@@ -11,10 +11,24 @@ fetch(
   .then((data) => {
     let temp = data["main"].temp;
     let name = data["name"];
+    let country=data["sys"].country;
     let descr = data["weather"][0]["description"];
-    document.querySelector(".description").innerHTML = descr;
+    let icon=data.weather[0].icon;
+    let temp_min=data.main.temp_min;
+    let temp_max=data.main.temp_max;
+    let humidity=data.main.humidity;
+    let wind=data.wind.speed;
+
+    
+
+    document.querySelector(".desc").innerHTML = descr;
     document.querySelector(".temp").innerHTML = temp+"°C";
-    document.querySelector(".name").innerHTML = name;
+    document.querySelector("#title").innerHTML=name +" , "+country;
+    document.querySelector(".ico").src = `https://amindi.ge/static/img/${icon[0]+icon[1]}.png`;
+    document.querySelector("#max").innerHTML+=temp_max +"°C";
+    document.querySelector("#min").innerHTML+=temp_min +"°C";
+    document.querySelector("#humidity").innerHTML+=humidity+" %";
+    document.querySelector("#wind").innerHTML+=wind+" m/s"
     input_list.push(name);
   });
 
@@ -27,13 +41,15 @@ button.addEventListener("click", () => {
     .then((data) => {
       var cod = data["cod"];
       if(cod==200){
-   
-    
-      var timez =data.timezone;
-      var temp = data["main"].temp;
-      var name = data["name"];
-      var descr = data["weather"][0]["description"];
-      var cod = data["cod"];
+        let temp = data["main"].temp;
+        let name = data["name"];
+        let country=data["sys"].country;
+        let descr = data["weather"][0]["description"];
+        let icon=data.weather[0].icon;
+        let temp_min=data.main.temp_min;
+        let temp_max=data.main.temp_max;
+        let humidity=data.main.humidity;
+        let wind=data.wind.speed;
       
         if (input_list.includes(name)) {
           input.style.boxShadow = "1px 1px 5px red";
@@ -45,18 +61,37 @@ button.addEventListener("click", () => {
 
           input_list.push(name);
           container.innerHTML += `
-      <div class="container">
-       <div class="city">
-          <div class="left">
-             <span class="description">${descr}</span>
-             <span class="temp">${temp}°C</span>
+          <div class="container">
+          <div class="city">
+            <div class="title">
+              <span id="title"> ${name} , ${country}</span>
+            </div>
+            <div class="information">
+              <div class="left">
+                <span class="temp">${temp} °C</span>
+                <div class="description">
+                   <img class="ico" src=https://amindi.ge/static/img/${icon[0]+icon[1]}.png >
+                   <span class="desc">${descr}</span>  
+                 </div>
+              </div>
+              <div class="right">
+                <div id="wind"> wind : ${wind} m/s</div>
+                <div class="humadity">
+                  <span id="humidity">humidity : ${humidity} %</span>
+              </div>
+              </div>
+              <div class="max_min">
+                  <div class="max">  
+                      <span id="max"><b>↑</b> ${temp_max} °C</span>
+                  </div>
+                  <div class="min">  
+                      <span id="min"><b>↓</b>  ${temp_min} °C </span>
+                  </div>
+                  
+              </div>
+            </div>
           </div>
-          <div class="right">
-             <span class="weekday"></span>
-             <span class="name">${name}</span>
-          </div>
-       </div>
-      </div> `;
+        </div> `;
 
       input.value="";
         }
